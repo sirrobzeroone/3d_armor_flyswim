538234b602293f8d3b316dde1db7fd8e9dd132b0
mazes-80 <>
2021-09-27 07:36:26 +0200

    ndraw: local

--- i_functions.lua
+++ i_functions.lua
@@ -52,6 +52,7 @@ local compare = draw_ta
 		compare = draw_tl
 	end	
 		
+	local_ndraw
 	for k,v in pairs(nodes) do
 		n_draw = minetest.registered_nodes[v.name].drawtype
 			for k2,v2 in ipairs(compare) do 
